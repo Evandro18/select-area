b2cab51ac3b5f6c200b06dbcab04d62e64538575
Evandro <evandro18.goncalves@gmail.com>
2020-01-19 21:32:20 -0400

    Updates

---
+++ precache-manifest.8dde5963b8cbb3f807b0882bc01bffcc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255f981e2579b91b84f936de86becb17",
+    "url": "/select-area/index.html"
+  },
+  {
+    "revision": "9285a0abf8717bdc19cf",
+    "url": "/select-area/static/css/main.34169ada.chunk.css"
+  },
+  {
+    "revision": "f0b38213523df02997e8",
+    "url": "/select-area/static/js/2.38c0ec67.chunk.js"
+  },
+  {
+    "revision": "9285a0abf8717bdc19cf",
+    "url": "/select-area/static/js/main.4f5bb01e.chunk.js"
+  },
+  {
+    "revision": "aadf38bf51611f2d68ca",
+    "url": "/select-area/static/js/runtime~main.27bec05a.js"
+  }
+]);
